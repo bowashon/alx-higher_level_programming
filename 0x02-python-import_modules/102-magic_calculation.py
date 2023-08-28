29236c3614f28e61f6076ed5916d19e026a0ba92
bowashon <baidoofelix05@gmail.com>
2023-08-28 05:46:05 +0000

    changes made

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import dis
+
+
 def magic_calculation(a, b):
     from my_magic_calculation import add, sub
     if a < b:
@@ -8,3 +11,5 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
+
+dis.dis(magic_calculation)
