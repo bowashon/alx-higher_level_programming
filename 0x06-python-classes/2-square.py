050c3b757198c898476c47e225d48e420dd7f0af
bowashon <baidoofelix05@gmail.com>
2023-08-30 03:03:22 +0000

    class with size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""create a class Square with attributes and instantiation with raise Errors"""
+
+
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
