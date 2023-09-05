078d612120ae024ab931121c48427ff2265507dd
bowashon <baidoofelix05@gmail.com>
2023-09-05 10:59:13 +0000

    changes made

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-    
+
     @property
     def width(self):
         return self.__width
@@ -43,7 +43,7 @@ class Rectangle:
 
     def area(self):
         return self.__width * self.__height
-    
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
