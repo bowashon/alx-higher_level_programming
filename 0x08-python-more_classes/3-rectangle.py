9add391ef950c0af98931fbcb0cdd516f90141d5
bowashon <baidoofelix05@gmail.com>
2023-09-05 19:16:34 +0000

    changes made

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""To define a class with str definition"""
+
+
 class Rectangle:
+    """
+    Defines the class with
+    Attributes
+    ----------
+    with and height
+    """
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
