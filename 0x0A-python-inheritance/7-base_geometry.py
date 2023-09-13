a83b9e566ab6f98608ba8aa86c2908ae1adc28ae
bowashon <baidoofelix05@gmail.com>
2023-09-13 21:06:35 +0000

    changes made

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
 
     def area(self):
         """Define an instance method area"""
-        raise("area() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
