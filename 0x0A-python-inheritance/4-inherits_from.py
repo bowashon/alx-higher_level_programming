e2f9159c49ed9ad9709025fe1bc9ed5bf7a4b3c9
bowashon <baidoofelix05@gmail.com>
2023-09-13 21:03:30 +0000

    changes made

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,7 +13,7 @@ def inherits_from(obj, a_class):
         a_class: the class to compare
     Return: returns true or false
     """
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
