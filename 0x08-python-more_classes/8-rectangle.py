eaf08871c41c3509f105e0f68def0687a96a8d1d
bowashon <baidoofelix05@gmail.com>
2023-09-17 04:37:25 +0000

    changes made

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
            is deleted
         """
         print("Bye rectangle...")
-        self.number_of_instances -= 1;
+        self.number_of_instances -= 1
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
