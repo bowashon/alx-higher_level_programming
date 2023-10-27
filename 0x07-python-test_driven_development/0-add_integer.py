91d4bfc26104c81800ee9bae9f1fea49563bdf1d
bowashon <baidoofelix05@gmail.com>
2023-10-27 07:33:41 +0000

    first test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+"""function that adds two integers"""
+
+
+def add_integer(a, b=98):
+    """Defines a function that adds two integers"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,5 @@
+#0x07-python-test-driven-development
+this is an exercise folder for alx student.
+It is based on the ability of the student to tests their code
+Test driven development helps the student to understand the need to write 
+simple but important test to test their code
