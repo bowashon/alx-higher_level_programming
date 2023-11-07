5954848f7dd7de5ea1e37e0bf3967db076482765
bowashon <baidoofelix05@gmail.com>
2023-11-07 22:18:20 +0000

    json to string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""Returns JSON representation of abj"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns:
+    returns JSON representation of an object
+    """
+    return (json.dumps(my_obj))
