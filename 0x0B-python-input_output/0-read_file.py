7e19a7acfd1f7ffdb8088b4f2c998d1f70f83544
bowashon <baidoofelix05@gmail.com>
2023-11-07 17:28:21 +0000

    read_file function

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""read file module"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file(UTF8)
+    prints - prints to the stdout
+    """
+    with open(filename, 'r', encoding="UTF8") as f:
+        read_file = f.read()
+        print(read_file)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,4 @@
+0x0B. Python- Input/Output
+This test is part of Alx software Engineering exercise 
+This exercise is meant to help student evaluate their
+understanding of Python - Input/Output on files
