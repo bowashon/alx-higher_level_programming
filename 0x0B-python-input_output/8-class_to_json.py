1ae2a9ff68810c1f2683af0d0e38fcd075fd38d7
bowashon <baidoofelix05@gmail.com>
2023-11-08 09:27:05 +0000

    dictionary description added

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#1/usr/bin/python3
+""" Return dictionary description module"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description fo 
+    a JSON serialization of an object
+    """
+    return obj.__dict__
