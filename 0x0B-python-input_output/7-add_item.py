6f58b0869b8af126a1ddb820c0ad880e2a2dee10
bowashon <baidoofelix05@gmail.com>
2023-11-08 05:39:47 +0000

    add_item module

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+import sys
+"""the add item module"""
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = list(sys.argv[1:])
+
+try:
+    data = load_from_json_file('add_item.json')
+except Exception:
+    data = []
+
+data.extend(args)
+save_to_json_file(data, 'add_item.json')
