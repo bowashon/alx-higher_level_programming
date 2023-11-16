72698a346f6434cb0fadd94b7c7d8f4e4fdfe0f0
bowashon <baidoofelix05@gmail.com>
2023-11-16 14:45:08 +0000

    inherits_from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""inherits_from module"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an istance of the
+    class that inherited (directly or indirectly) from
+    a specified class; otherwise False
+    """
+    return (type(obj) == a_class)
