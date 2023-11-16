fb82b3f26e1aea167809eae769e29e7944b52411
bowashon <baidoofelix05@gmail.com>
2023-11-16 12:53:43 +0000

    1-My_list class

---
+++ 0x0A-python-inheritance/1-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList([-10, 15, -29, 12, 0])
+
+
+print(my_list.print_sorted())
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class MyList"""
+
+
+class MyList(list):
+    """
+    Defines class Mylist that inherits from list
+    """
+    def print_sorted(self):
+        """
+        public method that prints the list
+        but sorted (ascending sort)
+        """
+        return (sorted(self))
