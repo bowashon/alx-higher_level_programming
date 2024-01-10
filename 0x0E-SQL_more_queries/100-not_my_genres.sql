832606a5cb1154e223dabe5cb74974516367f1c9
bowashon <baidoofelix05@gmail.com>
2024-01-10 13:01:38 +0000

    not Dexter

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- script that list all genres not linked to the show DExter
+SELECT DISTINCT name FROM tv_genres
+    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_genres.name NOT IN (
+	SELECT name FROM tv_genres
+	INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_shows.title = 'Dexter')
+ORDER BY tv_genres.name ASC;
