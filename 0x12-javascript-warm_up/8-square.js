323fb3e6144d6d791db8ce21afe036ca4b1db8bb
bowashon <baidoofelix05@gmail.com>
2024-01-12 14:02:05 +0000

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const i = process.argv[2];
+
+if (i !== undefined) {
+  if (!isNaN(parseInt(i))) {
+    for (let a = 0; a < i; a++) {
+      let row = '';
+      for (let b = 0; b < i; b++) {
+        row += 'x';
+      }
+      console.log(row);
+    }
+  } else {
+    console.log('Missing size');
+  }
+} else {
+  process.exit();
+}
