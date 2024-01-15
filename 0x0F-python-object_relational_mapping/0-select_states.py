e653cab3ae90a4ec86f5a6719e6b8f85ba0e75b0
bowashon <baidoofelix05@gmail.com>
2024-01-15 07:34:23 +0000

    select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# A script that lists all states from the database hbtn_0e_0_use
+# connect to database through mysql username, mysql password, and database name
+# Usage: ./0-select_states.sql <mysql username>, <mysql password> <database name>
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    my_cursor = db.cursor()
+    my_cursor.execute("SELECT * FROM states")
+    [print(state) for state in my_cursor.fetchall()]
+    my_cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,3 @@
+0x0F. Python - Object-relational mapping
+This directory is a test exercise for ALX EDUCATION that test pupils understanding
+of object-relation database
